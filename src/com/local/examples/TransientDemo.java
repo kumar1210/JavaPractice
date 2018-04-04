31bfa9b7ee30ed5ec4d3bc98198ad44e0a05fb3e
gaurav's <gaurav's@champ>
2018-04-04 09:39:25 +0530

    transient example

---
+++ src/com/local/examples/TransientDemo.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package com.local.examples;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+/**
+ * @author gaurav's
+ *
+ */
+public class TransientDemo implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	private String firstName;
+    private transient String middleName;
+    private String lastName;
+
+    public TransientDemo (String fName, String mName, String lName){
+        this.firstName = fName;
+        this.middleName = mName;
+        this.lastName = lName;
+    }
+
+    public String toString(){
+        StringBuffer sb = new StringBuffer(40);
+        sb.append("First Name : ");
+        sb.append(this.firstName);
+        sb.append(" Middle Name : ");
+        sb.append(this.middleName);
+        sb.append(" Last Name : ");
+        sb.append(this.lastName);
+        return sb.toString();
+    }
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		TransientDemo nameStore = new TransientDemo("Steve", "Middle","Jobs");
+		try {
+	        ObjectOutputStream o = new ObjectOutputStream(new FileOutputStream("nameStore"));
+	        // writing to object
+	        o.writeObject(nameStore);
+	        o.close();
+	        
+	        ObjectInputStream in = new ObjectInputStream(new FileInputStream("nameStore"));
+	        TransientDemo nameStore1 = (TransientDemo)in.readObject();
+	        System.out.println(nameStore1);
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+	}
+
+}
---
+++ src/com/local/generics/GenericsDemo.java
@@ -0,0 +1,40 @@
+package com.local.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsDemo {
+
+	public static void main(String[] args) {
+
+		List<Integer> list = new ArrayList<>();
+		list.add(4);
+		list.add(1);
+		
+		for (Number number : list) {
+			System.out.println(number);
+		}
+		
+		nonGenericmethod1((List)list);
+		genericMethod1(list);
+	}
+
+	/***
+	 * 
+	 * @param list
+	 */
+	public static void nonGenericmethod1(List<Number> list) {
+		for (Number number : list) {
+			System.out.println(number);
+		}
+	}
+	/***
+	 * wildcard upper bound example
+	 * @param list
+	 */
+	public static void genericMethod1(List<? extends Number> list) {
+		for (Number number : list) {
+			System.out.println(number);
+		}
+	}
+}
